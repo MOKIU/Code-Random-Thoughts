d3317df95f4aa625af6e0edfafa1851e8414aa57
MOKIU <3172921993@qq.com>
2025-08-27 15:29:35 +0800

    移除链表元素

--- LinkedList/P203 移除链表元素/P203.cpp
+++ LinkedList/P203 移除链表元素/P203.cpp
@@ -1,29 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode{
-	int val;
-	ListNode* next;
-	ListNode():val(0),next(nullptr);
-	ListNode(int val):val(val),next(nullptr);
-	ListNode(int val,ListNode* next):val(val),next(nullptr);
-}
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {};
+    ListNode(int val) : val(val), next(nullptr) {};
+    ListNode(int val, ListNode* next) : val(val), next(next) {};
+};
 
-ListNode* removeElements(ListNode* head,int val){
+ListNode* removeElements(ListNode* head, int val)
+{
     ListNode *p = head, *q = head;
-    while (head != nullptr && head->val == val) {
+    while (head != nullptr && head->val == val)
+    {
         p = head->next;
         delete head;
         head = p;
     }
-    if (p != nullptr) {
+    if (p != nullptr)
+    {
         q = p->next;
-        while (q != nullptr) {
-            if (q->val == val) {
+        while (q != nullptr)
+        {
+            if (q->val == val)
+            {
                 p->next = q->next;
                 delete q;
                 q = p->next;
-            } else {
+            }
+            else
+            {
                 p = p->next;
                 q = q->next;
             }
@@ -32,6 +40,32 @@ ListNode* removeElements(ListNode* head,int val){
     return head;
 }
 
-int main(){
-	return 0;
+// 初始化链表函数
+ListNode* initLinkedList(int* arr, int size)
+{
+    if (size == 0)
+    {
+        return nullptr;
+    }
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* current = head;
+    for (int i = 1; i < size; i++)
+    {
+        current->next = new ListNode(arr[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+int main()
+{
+    int data[] = {1, 3, 3, 4};
+    ListNode* head = initLinkedList(data, 4);
+    ListNode* p = removeElements(head, 3);
+    while (p != nullptr)
+    {
+        cout << p->val << " ";
+        p = p->next;
+    }
+    return 0;
 }
