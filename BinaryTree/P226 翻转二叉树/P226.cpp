bfea893b6143edda981d5f17c597f8a4ff6239fd
MOKIU <3172921993@qq.com>
2025-12-09 22:07:19 +0800

    12月9日更新

---
+++ BinaryTree/P226 翻转二叉树/P226.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {};
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {};
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {};
+};
+// 左右翻转二叉树
+TreeNode* invertTree(TreeNode* root){
+	if(root == nullptr)return nullptr;
+	TreeNode *node = root;
+	TreeNode *tmp = root->left;
+	root->left = root->right;
+	root->right = tmp;
+	invertTree(root->left);
+	invertTree(root->right);
+	return root;
+}
+int main(){
+	return 0;
+}
