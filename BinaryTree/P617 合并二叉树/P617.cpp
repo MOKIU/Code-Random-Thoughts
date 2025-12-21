c5b11043db4ff6cee176c71b29d9eefaa3a5f804
MOKIU <3172921993@qq.com>
2025-12-21 21:13:09 +0800

    12月21日更新

---
+++ BinaryTree/P617 合并二叉树/P617.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right);
+};
+TreeNode* traversal(TreeNode*& cur1, TreeNode*& cur2){
+	if(cur1 == nullptr)return cur2;
+	if(cur2 == nullptr)return cur1;
+	cur1->val += cur2->val;
+	cur1->left = traversal(cur1->left, cur2->left);
+	cur1->right = traversal(cur1->right, cur2->right);
+	return cur1;
+}
+TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2){
+	return traversal(root1, root2);
+}
+int main(){
+	return 0;
+}
