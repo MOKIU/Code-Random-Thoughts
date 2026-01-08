f1f965d11b58de71a03d5c31a5de647893b8bedc
MOKIU <3172921993@qq.com>
2026-01-08 22:52:11 +0800

    1月8日更新

---
+++ BackTracking/P216 组合总和3/P216.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> result;
+vector<int> path;
+void backtracking(int k, int n, int startIndex, int sum){
+	if(path.size() == k){
+		if(sum == n){
+			result.push_back(path);
+		}
+		return;
+	}
+	for(int i = startIndex;i <= 9;i++){
+		path.push_back(i);
+		sum += i;
+		backtracking(k, n, i+1, sum);
+		sum -= i;
+		path.pop_back();	
+	}
+}
+vector<vector<int>> combinationSum3(int k, int n){
+	backtracking(k, n, 1, 0);
+	return result;
+}
+int main(){
+	return 0;
+}
