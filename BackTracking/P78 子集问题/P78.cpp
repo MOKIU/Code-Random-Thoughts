1a5adf0da4ec0a6231fc30b63df6a4cc394f519d
MOKIU <3172921993@qq.com>
2026-01-16 23:14:12 +0800

    1月16日更新

--- BackTracking/P78 子集问题/P78.cpp
+++ BackTracking/P78 子集问题/P78.cpp
@@ -3,16 +3,23 @@ using namespace std;
 
 vector<vector<int>> result;
 vector<int> path;
-void backtracking(vector<int> nums, int startIndex){
+void backtracking(vector<int> nums, int startIndex, vector<bool>& used){
 	result.push_back(path);
 	for(int i = startIndex;i < nums.size();i++){
+		if(i > 0 && nums[i] == nums[i-1] && used[i-1] == false){
+			continue;
+		}
 		path.push_back(nums[i]);
-		backtracking(nums, i+1);
+		used[i] = true;
+		backtracking(nums, i+1, used);
+		used[i] = false;
 		path.pop_back();
 	}
 }
 vector<vector<int>> subsets(vector<int>& nums){
-	backtracking(nums, 0);
+	vector<bool> used(nums.size(), false);
+	sort(nums.begin(), nums.end());
+	backtracking(nums, 0, used);
 	return result;
 }
 int main(){
---
+++ BackTracking/P90 子集2/P90.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> result;
+vector<int> path;
+
+vector<vector<int>> subsetWithDup(vector<int>& nums){
+	
+}
+int main(){
+	return 0;
+}
