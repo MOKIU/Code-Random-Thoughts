55f057530dad2fe7a63c1329d2539f632a77c891
MOKIU <3172921993@qq.com>
2026-01-17 23:19:25 +0800

    1月17日更新

---
+++ BackTracking/P46 全排列/P46.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> result;
+vector<int> path;
+void backtracking(vector<int>& nums, vector<bool>& used){
+	if(path.size() == nums.size()){
+		result.push_back(path);
+		return;
+	}
+	for(int i = 0; i < nums.size();i++){
+		if(used[i] == true)continue;
+		used[i] = true;
+		path.push_back(nums[i]);
+		backtracking(nums, used);
+		path.pop_back();
+		used[i] = false;
+	}
+}
+vector<vector<int>> permute(vector<int>& nums){
+	vector<bool> used(nums.size(), false);
+	backtracking(nums, used);
+	return result;
+}
--- BackTracking/P90 子集2/P90.cpp
+++ BackTracking/P90 子集2/P90.cpp
@@ -3,9 +3,24 @@ using namespace std;
 
 vector<vector<int>> result;
 vector<int> path;
-
+void backtracking(vector<int> nums, int startIndex, vector<bool>& used){
+	result.push_back(path);
+	for(int i = startIndex;i < nums.size();i++){
+		if(i > 0 && nums[i] == nums[i-1] && used[i-1] == false){
+			continue;
+		}
+		path.push_back(nums[i]);
+		used[i] = true;
+		backtracking(nums, i+1, used);
+		used[i] = false;
+		path.pop_back();
+	}
+}
 vector<vector<int>> subsetWithDup(vector<int>& nums){
-	
+	vector<bool> used(nums.size(), false);
+	sort(nums.begin(), nums.end());
+	backtracking(nums, 0, used);
+	return result;
 }
 int main(){
 	return 0;
